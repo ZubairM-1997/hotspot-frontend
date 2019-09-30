23f38c0bd48cf7f72c743de6410cd95c8aa1d60d
Zubair <zubair.maqsood6@gmail.com>
2019-09-30 01:04:04 +0100

    edit weather api

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -18,7 +18,7 @@ export default class Weather extends React.Component {
 
 	componentDidUpdate(prevProps, prevState){
 		if (this.props.weather.lat && prevProps.weather.lat !== this.props.weather.lat) {
-			fetch(`http://api.worldweatheronline.com/premium/v1/weather.ashx?key=d0d9ff2f5bce4647a9693045191807&q=${this.props.weather.lat},${this.props.weather.long}&format=json`)
+			fetch(`http://api.worldweatheronline.com/premium/v1/weather.ashx?key=acbec1d6ae0e477a95000157193009&q=${this.props.weather.lat},${this.props.weather.long}&format=json`)
 			.then(resp => resp.json())
 			.then((data) => this.temp({...data}))
 
